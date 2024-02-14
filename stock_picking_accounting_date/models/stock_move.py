c532b205c2b75c16cec26837dc778da7c48e754a
Yoshi Tashiro <tashiro@quartile.co>
2024-02-14 03:03:42 +0000

    [3694][FIX] stock_picking_accounting_date
    Before this commit, accounting date set in inventory adjustments would be nullified
    as the date of the account move would be overriden by the accounting date field of
    the originating stock move, which was missing consideration on the force_period_date
    context.
    
    This commit fixes the issue by respecting the context value in assigning the accounting
    date in stock moves.

--- stock_picking_accounting_date/models/stock_move.py
+++ stock_picking_accounting_date/models/stock_move.py
@@ -1,4 +1,4 @@
-# Copyright 2023 Quartile Limited
+# Copyright 2023-2024 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models
@@ -14,10 +14,20 @@ class StockMove(models.Model):
 
     @api.depends("date", "accounting_date")
     def _compute_accounting_date(self):
-        for line in self:
-            line.accounting_date = fields.Datetime.context_timestamp(self, line.date)
-            if line.picking_id.accounting_date:
-                line.accounting_date = line.picking_id.accounting_date
+        # 'force_period_date' context is assigned when user sets accounting date in
+        # inventory adjustment
+        force_period_date = self._context.get("force_period_date")
+        if force_period_date:
+            for line in self:
+                line.accounting_date = force_period_date
+        else:
+            for line in self:
+                if line.picking_id.accounting_date:
+                    line.accounting_date = line.picking_id.accounting_date
+                    continue
+                line.accounting_date = fields.Datetime.context_timestamp(
+                    self, line.date
+                )
 
     def _prepare_account_move_vals(
         self,
