edd31662b63aea3b23e14e017c05456e8bef653e
Aungkokolin1997 <lin@quartile.co>
2024-08-16 03:01:44 +0000

    reflect oca changes

--- stock_picking_accounting_date/models/stock_picking.py
+++ stock_picking_accounting_date/models/stock_picking.py
@@ -13,8 +13,8 @@ class StockPicking(models.Model):
     show_accounting_date = fields.Boolean(compute="_compute_show_accounting_date")
 
     def _compute_show_accounting_date(self):
+        self.show_accounting_date = False
         for pick in self:
-            pick.show_accounting_date = False
             if pick.picking_type_code not in ("incoming", "outgoing"):
                 continue
             if pick.move_ids.with_company(pick.company_id).product_id.filtered(
