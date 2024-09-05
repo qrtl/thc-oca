910bfd68e27ceb77b54b184aae2d231a3a4d596d
Aungkokolin1997 <lin@quartile.co>
2024-09-05 03:50:06 +0000

    [IMP] stock_picking_accounting_date: improve _compute_show_accounting_daate

--- stock_picking_accounting_date/models/stock_picking.py
+++ stock_picking_accounting_date/models/stock_picking.py
@@ -11,13 +11,30 @@ class StockPicking(models.Model):
         help="Accounting date for stock valuation journal entry.",
     )
     show_accounting_date = fields.Boolean(compute="_compute_show_accounting_date")
+    is_editable_accounting_date = fields.Boolean(
+        compute="_compute_is_editable_accounting_date", string="Is Editable"
+    )
+
+    def _compute_is_editable_accounting_date(self):
+        for record in self:
+            if self.env.user.has_group("stock.group_stock_manager"):
+                record.is_editable_accounting_date = True
+            else:
+                record.is_editable_accounting_date = record.state not in [
+                    "done",
+                    "cancel",
+                ]
 
     def _compute_show_accounting_date(self):
         self.show_accounting_date = False
         for pick in self:
-            if pick.picking_type_code not in ("incoming", "outgoing"):
-                continue
-            if pick.move_ids.with_company(pick.company_id).product_id.filtered(
-                lambda x: x.detailed_type == "product" and x.valuation == "real_time"
-            ):
+            valued_moves = pick.move_ids.with_company(pick.company_id).filtered(
+                lambda move: move.product_id.detailed_type == "product"
+                and move.product_id.valuation == "real_time"
+                and not (
+                    move.location_id._should_be_valued()
+                    and move.location_dest_id._should_be_valued()
+                )
+            )
+            if valued_moves:
                 pick.show_accounting_date = True
