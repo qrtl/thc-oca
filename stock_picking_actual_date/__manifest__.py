78d45a3eb92a2c48522f9aef84afedfb872e600e
Aungkokolin1997 <lin@quartile.co>
2024-09-11 07:02:56 +0000

    revert version

--- stock_picking_actual_date/__manifest__.py
+++ stock_picking_actual_date/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Stock Picking Actual Date",
-    "version": "16.0.1.0.1",
+    "version": "16.0.1.0.0",
     "author": "Quartile Limited, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
     "category": "Stock",
