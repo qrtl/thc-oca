a0418ca6666d14f50234db6e5fd8c505fa5f61a3
Aungkokolin1997 <lin@quartile.co>
2024-09-12 04:04:46 +0000

    adj

--- stock_picking_actual_date/models/__init__.py
+++ stock_picking_actual_date/models/__init__.py
@@ -1,3 +1,4 @@
+from . import account_move
 from . import res_currency
 from . import stock_picking
 from . import stock_move
--- stock_picking_actual_date/models/stock_picking.py
+++ stock_picking_actual_date/models/stock_picking.py
@@ -20,21 +20,6 @@ class StockPicking(models.Model):
             ):
                 rec.is_editable_actual_date = True
 
-    def _sequence_actual_date_matches(self, move, date):
-        sequence = move.name
-        format_values = move._get_sequence_format_param(sequence)[1]
-        sequence_number_reset = move._deduce_sequence_number_reset(sequence)
-        year_start, year_end = move._get_sequence_date_range(sequence_number_reset)
-        year_match = (
-            not format_values["year"]
-            or move._year_match(format_values["year"], year_start)
-        ) and (
-            not format_values["year_end"]
-            or move._year_match(format_values["year_end"], year_end)
-        )
-        month_match = not format_values["month"] or format_values["month"] == date.month
-        return year_match and month_match
-
     def write(self, vals):
         res = super().write(vals)
         if "actual_date" in vals and vals["actual_date"]:
@@ -46,10 +31,9 @@ class StockPicking(models.Model):
                     continue
                 account_moves.button_draft()
                 for move in account_moves:
-                    if self._sequence_actual_date_matches(move, rec.actual_date):
-                        move.date = rec.actual_date
-                    else:
+                    move = move.with_context(skip_date_sequence_check=True)
+                    move.date = rec.actual_date
+                    if not move._sequence_matches_date():
                         move.name = False
-                        move.date = rec.actual_date
                 account_moves.action_post()
         return res
