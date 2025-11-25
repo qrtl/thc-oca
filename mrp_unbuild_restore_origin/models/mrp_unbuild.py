853f0e458067e25b59f14adbef8389535ee7692c
nobuQuartile <nobu@quartile.co>
2025-11-25 07:30:02 +0000

    upd from lin-san OCA commit

--- mrp_unbuild_restore_origin/models/mrp_unbuild.py
+++ mrp_unbuild_restore_origin/models/mrp_unbuild.py
@@ -1,6 +1,8 @@
 # Copyright 2025 Quartile (https://www.quartile.co)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+from collections import defaultdict
+
 from odoo import fields, models
 
 
@@ -29,29 +31,6 @@ class MrpUnbuild(models.Model):
             vals["location_dest_id"] = origin_move_line.location_id.id
         return vals
 
-    def _get_move_line_vals(self, move, move_line):
-        vals = {
-            "move_id": move.id,
-            "owner_id": move_line.owner_id.id,
-            "qty_done": min(move.product_uom_qty, move_line.qty_done),
-            "product_id": move.product_id.id,
-            "product_uom_id": move.product_uom.id,
-            "location_id": move.location_id.id,
-            "location_dest_id": move.location_dest_id.id,
-            # When executing button_unbuild, the lot assigned to the MO is set in the
-            # context as default_lot_id. As a result, when creating related material
-            # stock_move_line records, the default_lot_ids is automatically used as
-            # the default lot_id, even before _action_assign explicitly assigns a lot.
-            # This can cause an error when the default lot does not match the product.
-            # Therefore, we explicitly specify the lot here to prevent using the default
-            # lot value.
-            "lot_id": move_line.lot_id.id,
-        }
-        if self.env.context.get("exact_location"):
-            vals["location_id"] = move_line.location_dest_id.id
-            vals["location_dest_id"] = move_line.location_id.id
-        return vals
-
     def _generate_produce_moves(self):
         """This logic may seem a bit complex, but it's necessary due to how the following
         standard code works:
@@ -73,6 +52,7 @@ class MrpUnbuild(models.Model):
                     unbuild.mo_id.product_qty, unbuild.product_uom_id
                 )
             )
+            qty_already_used = defaultdict(float)
             for raw_move in raw_moves:
                 move = unbuild._generate_move_from_existing_move(
                     raw_move,
@@ -82,9 +62,18 @@ class MrpUnbuild(models.Model):
                 )
                 if move.has_tracking == "none":
                     vals_list = []
+                    needed_quantity = move.product_uom_qty
                     for move_line in raw_move.move_line_ids:
-                        vals = self._get_move_line_vals(move, move_line)
-                        vals_list.append(vals)
+                        taken_quantity = min(
+                            needed_quantity,
+                            move_line.qty_done - qty_already_used[move_line],
+                        )
+                        if taken_quantity:
+                            vals = self._prepare_move_line_vals(
+                                move, move_line, taken_quantity
+                            )
+                            vals_list.append(vals)
+                            qty_already_used[move_line] += taken_quantity
                     self.env["stock.move.line"].create(vals_list)
                     move.write({"state": "confirmed"})
                 moves += move
